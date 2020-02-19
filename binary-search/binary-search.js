714d281ac75772acfdabc536f419dbfc7e78373c
thebubbleblower <carmenwen1992@gmail.com>
2020-02-19 16:58:09 -0500

    completed binary search with indexes

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,6 +2,57 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
+
+	let i = Math.floor(array.length/2)
+
+	let j;
+
+	let midpoint;
+
+	if(target > array[i]){
+		j = array.length - 1;
+		if( target > array[j]){
+			return false;
+		}
+	}
+
+	else if (target < array[i]){
+		j = 0;
+		if (target < array[j]){
+			return false;
+		}
+	}
+
+	while(Math.abs(i-j) !== 1){
+		if(target === array[i] || target === array[j]){
+			return true
+		}
+
+		midpoint = Math.floor((j + i)/2);
+
+		if (target === array[midpoint]){
+			return true;
+		}
+
+		if ( target < array[midpoint] ) {
+			if ( j < i ) {
+				i = midpoint;
+			}
+			else{
+				j = midpoint;
+			}
+		}
+
+		if ( target > array[midpoint] ) {
+			if ( j < i ) {
+				j = midpoint;
+			}
+			else {
+				i = midpoint;
+			}
+		}
+	}
+	return false
 	
 };
 
