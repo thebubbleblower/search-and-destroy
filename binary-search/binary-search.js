c215e42908da4737519e8e4395ac9111359b444b
thebubbleblower <carmenwen1992@gmail.com>
2020-02-19 19:18:08 -0500

    refactored binary search to be the best version

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -8,22 +8,22 @@ const binarySearch = (array, target) => {
 	}
 
 	let midpoint = Math.floor(array.length/2);
-	let min;
-	let max;
+	let min = 0;
+	let max = array.length - 1;
 
-	if ( array[midpoint] === target ) {
-		return true;
-	}
+	// if ( array[midpoint] === target ) {
+	// 	return true;
+	// }
 
-	if ( target < array[midpoint] ) {
-		min = 0;
-		max = midpoint;
-	}
+	// if ( target < array[midpoint] ) {
+	// 	min = 0;
+	// 	max = midpoint;
+	// }
 
-	if ( target > array[midpoint] ) {
-		min = midpoint;
-		max = array.length - 1;
-	}
+	// if ( target > array[midpoint] ) {
+	// 	min = midpoint;
+	// 	max = array.length - 1;
+	// }
 
 	if ( array[min] === target || array[max] === target ) {
 		return true;
